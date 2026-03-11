17c4a9da9d9b339d82572df47f866a9e5c7e3bc0
Carlos Mata <carlosmata@correo.ugr.es>
2026-03-11 17:09:40 +0000

    Refactor dim_customers to get data from fct_orders.

---
+++ models/marts/marketing/dim_customers.sql
@@ -0,0 +1,37 @@
+with customers as (
+    select * from {{ ref('stg_tpch_customers') }}
+),
+
+orders as ( 
+    -- ¡EL GRAN CAMBIO ESTÁ AQUÍ! 
+    -- Ahora leemos de la tabla de hechos, no del staging
+    select * from {{ ref('fct_orders') }}
+),
+
+customer_orders as (
+    select
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(total_price) as lifetime_value
+
+    from orders
+    group by 1
+),
+
+final as (
+    select
+        customers.customer_id,
+        customers.name as customer_name,
+        customers.market_segment,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
+
+    from customers
+    left join customer_orders using (customer_id)
+)
+
+select * from final
